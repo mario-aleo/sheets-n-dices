6b6a965a3f7ac56faacca64ee9267dbce383ecf1
Mario Aleo <marioaleo@gmail.com>
2017-06-01 20:35:46 -0300

    Update Scrolls

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -2,7 +2,8 @@ module.exports = {
   staticFileGlobs: [
     '/index.html',
     '/manifest.json',
-    '/bower_components/webcomponentsjs/webcomponents-lite.min.js'
+    '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
+    '/bower_components/web-animations-js/web-animations-next-lite.min.js'
   ],
   navigateFallback: '/index.html',
   navigateFallbackWhitelist: [/^(?!\/__)/]
