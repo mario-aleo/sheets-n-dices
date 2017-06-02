d3be8acac769d9ad33b52b4718ca3d9cd01acbc6
Mario Aleo <marioaleo@gmail.com>
2017-06-01 21:17:15 -0300

    Hotfix IndexedDB Erro on Polymer Build

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -3,7 +3,9 @@ module.exports = {
     '/index.html',
     '/manifest.json',
     '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
-    '/bower_components/web-animations-js/web-animations-next-lite.min.js'
+    '/bower_components/web-animations-js/web-animations-next-lite.min.js',
+    '/bower_components/app-storage/app-indexeddb-mirror/common-worker-scope.js',
+    '/bower_components/app-storage/app-indexeddb-mirror/app-indexeddb-mirror-worker.js'
   ],
   navigateFallback: '/index.html',
   navigateFallbackWhitelist: [/^(?!\/__)/]
